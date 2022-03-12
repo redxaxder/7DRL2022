dacde5515c55e9fb89a4d0b78be3e4dc277ce3b4
redxaxder <abyaly@gmail.com>
2022-03-12 11:01:28 -0600

    fix whip

--- lib/attacks/whip.gd
+++ lib/attacks/whip.gd
@@ -9,43 +9,41 @@ func try_attack(ls: LocationService, pos: Vector2, dir: int, anim_delay: float, 
 	var leftv = DIR.dir_to_vec(left)
 	var right = DIR.invert(left)
 	var rightv = DIR.dir_to_vec(right)
-	var targets = [\
-		forward, \
-		forward + leftv , \
-		forward + (2 * leftv), \
-		forward + rightv , \
-		forward + (2 * rightv), \
-		]
-	var dirs = [dir, left, right, left, right]
+	var target
 
 	#forward
-	attacked = .try_attack_at(ls, targets[0], dirs[0], anim_delay)
+	target = forward
+	attacked = .try_attack_at(ls, target, dir, anim_delay)
 	if attacked:
-		spawn_indicator(targets[0])
+		spawn_indicator(target)
 		return attacked
-	if ls.lookup(targets[0], constants.STOPS_ATTACK).size() > 0 || terrain.is_wall(targets[0]):
+	if ls.lookup(target, constants.STOPS_ATTACK).size() > 0 || terrain.is_wall(target):
 		return attacked
 	#left1
-	attacked = .try_attack_at(ls, targets[1], dirs[1], anim_delay)
+	target = forward + leftv
+	attacked = .try_attack_at(ls, target, left, anim_delay)
 	if attacked:
-		spawn_indicator(targets[1])
+		spawn_indicator(target)
 		return attacked
 	#right1
-	attacked = .try_attack_at(ls, targets[2], dirs[2], anim_delay)
+	target = forward + rightv
+	attacked = .try_attack_at(ls, target, right, anim_delay)
 	if attacked:
-		spawn_indicator(targets[2])
+		spawn_indicator(target)
 		return attacked
 	#left2
-	if !(ls.lookup(targets[1], constants.STOPS_ATTACK).size() > 0 || terrain.is_wall(targets[1])):
-		attacked = .try_attack_at(ls, targets[3], dirs[3], anim_delay)
+	target = forward + (2 * leftv)
+	if !(ls.lookup(forward + leftv, constants.STOPS_ATTACK).size() > 0 || terrain.is_wall(forward + leftv)):
+		attacked = .try_attack_at(ls, target, left, anim_delay)
 		if attacked:
-			spawn_indicator(targets[3])
+			spawn_indicator(target)
 			return attacked
 	#right2
-	if !(ls.lookup(targets[2], constants.STOPS_ATTACK).size() > 0 || terrain.is_wall(targets[2])):
-		attacked = .try_attack_at(ls, targets[4], dirs[4], anim_delay)
+	target = forward + (2 * rightv)
+	if !(ls.lookup(forward + rightv, constants.STOPS_ATTACK).size() > 0 || terrain.is_wall(forward + rightv)):
+		attacked = .try_attack_at(ls, target, right, anim_delay)
 		if attacked:
-			spawn_indicator(targets[4])
+			spawn_indicator(target)
 			return attacked
 	return attacked
 #
