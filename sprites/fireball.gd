71539b912f566f15fc230921f92c9836a64f7bf5
Zachary Sarver <Zachary.Sarver@gmail.com>
2022-03-13 12:14:37 -0400

    flickering fireballs

---
+++ sprites/fireball.gd
@@ -0,0 +1,19 @@
+extends Sprite
+
+const anim_frames: int = 7
+var anim_frame: int = 0
+var red: bool = true
+
+func _process(delta: float) -> void:
+	update()
+
+func _draw() -> void:
+	if red:
+		self.modulate = Color(1, 0, 0)
+	else:
+		self.modulate = Color(1, 1, 0)
+	anim_frame = min(anim_frames, anim_frame + 1)
+	if anim_frame == anim_frames:
+		anim_frame = 0
+		red = !red
+
